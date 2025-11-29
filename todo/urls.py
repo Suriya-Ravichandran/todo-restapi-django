4adc21989fa7aa2e29024d22f10379727d46c695
suriyaravichandran <rsuriya119@gmail.com>
2025-11-29 15:25:09 +0530

    api completed

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('createtodo',views.createtodo,name="createtodo"),
-    path('showtodos',views.showtodos,name="showtodos")
+    path('showtodos',views.showtodos,name="showtodos"),
+    path('getonetodo/<int:pk>',views.getonetodo,name="getonetodo"),
+    path('updatetodo/<int:pk>',views.updatetodo,name="updatetodo"),
+    path('deletetodo/<int:pk>',views.deletetodo,name="deletetodo"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -21,3 +21,35 @@ def showtodos(request):
         todos=Todo.objects.all()
         serializer=TodoSerializer(todos,many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def getonetodo(request,pk):
+    try:    
+        todos=Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'error': 'Todo Not found'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method=='GET':
+        serializer=TodoSerializer(todos)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+def updatetodo(request,pk):
+    try:    
+        todos=Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'error': 'Todo Not found'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method=='PUT':
+         serializer = TodoSerializer(todos,data=request.data)
+         if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['DELETE'])
+def deletetodo(request,pk):
+    try:    
+        todos=Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'error': 'Todo Not found'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method=='DELETE':
+        todos.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
